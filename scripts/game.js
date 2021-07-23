efab38024e3d149f387ed4026232fe648dd5d1cf
Igu <61991921+mit-igu@users.noreply.github.com>
2021-07-23 12:23:51 -0300

    Uploading project

---
+++ scripts/game.js
@@ -0,0 +1,62 @@
+//game variables
+let board = [];
+//filling board variable
+if (board.length == 0) {
+    for (i = 0; i < 9; i++) {
+        board[i] = "";
+    }
+}
+//turno dos jogadores ( 0 e 1)
+let playerTurn = 0;
+//game symbols
+let symbols = ["o", "x"];
+//game status
+let gameOver = false;
+
+function handleMove(position) {
+    board[position] = symbols[playerTurn];
+    isGameOver();
+    if (!gameOver) {
+        console.log(board, playerTurn);
+        playerTurn = (playerTurn == 0) ? 1 : 0;
+    }
+
+}
+
+function isGameOver() {
+    let winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    for (i = 0; i < winConditions.length; i++) {
+        isWin(winConditions[i][0], winConditions[i][1], winConditions[i][2]);
+    }
+    if (board.every((position) => position != "") && gameOver == false) {
+        gameOver = true;
+        alert("Game Over. It's a tie.")
+        resetGame();
+    }
+}
+
+function isWin(pos1, pos2, pos3) {
+    if (board[pos1] == board[pos2] && board[pos1] == board[pos3] && board[pos1] != "") {
+        gameOver = true;
+    }
+}
+
+
+function resetGame() {
+    for (i = 0; i < 9; i++) {
+        board[i] = "";
+    }
+    playerTurn = 0;
+    gameOver = false;
+    console.log(board, playerTurn)
+}
---
+++ scripts/interface.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let squares = document.querySelectorAll('.square');
+
+    squares.forEach((square) => {
+        square.addEventListener('click', handleClick);
+    })
+
+    function handleClick(square) {
+        if (board[square.target.id] == "") {
+            updateSquare(square);
+            handleMove(square.target.id);
+            setTimeout(() => {
+                if (gameOver) {
+                    resetInterface();
+                    resetGame();
+                    alert(`Game Over. "${symbols[playerTurn].toUpperCase()}" wins`);
+                }
+            }, 10)
+
+
+        }
+    }
+
+    function updateSquare(square) {
+        let updating = square.target;
+        updating.innerHTML = `<div class="${symbols[playerTurn]}"></div>`
+    }
+
+    function resetInterface() {
+        squares.forEach((square) => {
+            square.innerHTML = "";
+        })
+    }
+
+    document.querySelector('#reset').addEventListener('click', resetGame);
+    document.querySelector('#reset').addEventListener('click', resetInterface);
+
+})
